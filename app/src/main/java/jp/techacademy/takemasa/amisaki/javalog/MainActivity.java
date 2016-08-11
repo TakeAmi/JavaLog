0886499f6af9764adae099b8138742d553e4747e
amisaki <t.amisaki0918@gmail.com>
2016-08-11 15:41:06 +0900

    課題提出用に微修正

--- app/src/main/java/jp/techacademy/takemasa/amisaki/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/takemasa/amisaki/javalog/MainActivity.java
@@ -11,10 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Human human = new Human("網崎",28,"JAVAのコーディング");
+        Human human1 = new Human("網崎", 28);
 
-        human.say();
-        human.think();
+        Human human2 = new Human("あみさき",5,"JAVAのコーディング");
+
+        human1.say(); //human1.think()は hobby を定義していないので使えない。
+
+        human2.say();
+        human2.think();
     }
 }
 
